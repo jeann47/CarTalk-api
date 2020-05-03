f458559732b1e37aadb9a7fef264d28db05a77c9
Jeann Carlos <jeann.carlos4@gmail.com>
2020-05-03 11:46:14 -0300

    setup and user migrations

---
+++ src/database/migrations/1588342741502-users.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class Users1588342741502 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password_hash',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+}
